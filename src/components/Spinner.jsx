a43aa2f3257d3923b8274993c9f2c4867038030e
Coco <coco@3sam.io>
2024-07-25 16:56:34 +0700

    add JSON mock server and working with forms

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,18 @@
+import ClickLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+const Spinner = ({ loading }) => {
+  return (
+    <ClickLoader
+      color="#4338ca"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+};
+
+export default Spinner;
