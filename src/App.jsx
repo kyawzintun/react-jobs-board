59e4470b41debc767c59c51eb0b5f00dbc042f61
Coco <coco@3sam.io>
2024-07-26 15:27:03 +0700

    implement to edit the job and added react-toastify package

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,34 @@ import NotFoundPage from './pages/NotFoundPage';
 import AddJobPage from './pages/AddJobPage';
 import MainLayout from './layouts/MainLayout';
 import JobPage, { jobLoader } from './pages/JobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   const addJob = async job => {
-    const res = await fetch('/api/jobs', {
+    await fetch('/api/jobs', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  const updateJob = async job => {
+    await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  const deleteJob = async id => {
+    await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
     });
     return;
   };
@@ -29,7 +48,16 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
